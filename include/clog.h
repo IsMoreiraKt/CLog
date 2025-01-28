42192e21dc12a80c7e1c63e44f29dfe4c40c234e
IsMoreiraKt <ismaelmoreirakt@gmail.com>
2025-01-28 07:17:03 +0000

    feat: Added structure to store logger settings.

--- include/clog.h
+++ include/clog.h
@@ -15,4 +15,31 @@ typedef enum {
   LOG_LEVEL_TRACE     /**< Detailed trace messages for tracking. */
 } LogLevel;
 
+/**
+ * @struct LogConfig
+ * @brief Configuration structure for the logging system.
+ *
+ * @var LogConfig::log_to_stdout
+ * Indicates whether logs should be sent to standard output.
+ *
+ * @var LogConfig::log_to_stderr
+ * Indicates whether logs should be sent to standard error output.
+ *
+ * @var LogConfig::log_file_path
+ * Path to the file where logs will be stored.
+ *
+ * @var LogConfig::rotation_interval
+ * File rotation interval in minutes.
+ *
+ * @var LogConfig::ignored_domains
+ * Array of log domains to be ignored.
+ */
+typedef struct {
+  int log_to_stdout;   /**< Enable/disable logging to standard output. */
+  int log_to_stderr;   /**< Enable/disable logging to standard error output. */
+  char *log_file_path; /**< Path to the log file. */
+  int rotation_interval;  /**< Log file rotation interval (in minutes). */
+  char **ignored_domains; /**< List of domains to be ignored in logging. */
+} LogConfig;
+
 #endif // CLOG_H
